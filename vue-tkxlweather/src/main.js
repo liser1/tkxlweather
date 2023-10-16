a26af0706aa73615395c90c868bdb815d8c9588d
lzq <2389593248@qq.com>
2023-10-16 10:29:51 +0800

    changes

--- vue-tkxlweather/src/main.js
+++ vue-tkxlweather/src/main.js
@@ -1,14 +1,7 @@
-import './assets/main.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
 import App from './App.vue'
-import router from './router'
-
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-
+import axios from 'axios'
+const app=createApp(App)
+app.use(axios)
 app.mount('#app')
