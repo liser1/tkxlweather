1be6b5fb39322bad7e6170ba3ac74c696f1b02ec
lzq <2389593248@qq.com>
2023-10-30 15:29:03 +0800

    头部样式完善

--- vue-tkxlweather/src/components/Header.vue
+++ vue-tkxlweather/src/components/Header.vue
@@ -1,32 +1,61 @@
 <template>
   <div class="box">
     <div class="left">
-      天空旋律
+      <i>天空旋律</i>
     </div>
     <div class="right">
-
+      <p class="space">
+        <img src="../assets/space.png" alt="">
+        {{space.sheng+" "+space.shi}}
+        <input type="text" value="搜索城市天气">
+      </p>
     </div>
   </div>
 </template>
 <script>
+  export default {
+    data() {
+      return {
+        space: {
+          sheng: '湖南省',
+          shi: '长沙市'
+        }
+      }
+    }
+  }
 </script>
 <style scoped lang="less">
   .box {
     margin: 0 auto;
     width: 1200px;
     background-color: #65b1fa;
-    height: 80px;
+    height: 65px;
     display: flex;
-    justify-content: space-between;
     align-items: center;
+    color: white;
 
     .left {
       height: 40px;
       line-height: 40px;
-      font-size: 32px;
-      color: white;
+      font-size: 30px;
+      margin-right: auto;
     }
 
-    .right {}
+    .right {
+      img {
+        vertical-align: middle;
+        margin-right: 5px;
+      }
+
+      input {
+        width: 250px;
+        height: 20px;
+        padding: 5px 13px;
+        border: 0;
+        background: rgba(255, 255, 255, 0.2);
+        color: rgba(255, 255, 255, .7);
+        border-radius: 15px;
+      }
+    }
   }
 </style>
